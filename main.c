75092d261142ff89ce7194a5356f9eb203a7f9b8
Kishore02721 <kishore.k@yantravision.com>
2024-12-17 14:51:26 +0530

    Add function included

--- main.c
+++ main.c
@@ -1,8 +1,21 @@
 //main created by Hariharan.S
 #include<stdio.h>
+
+int add(int input_1,int input_2); //Function to add two numbers
+
 int main(){
-   int input_1=30;
-   int input_2=2;
+int input_1=30;
+int input_2=2;
+int result=0;
+result=add(input_1,input_2);
+printf("Addition of two inputs=%d",&result);
+return 0;
+}
+
+int add(int input_1,int input_2){
+int result=0;
+result=input_1+input_2;
+return result;
 }
 
 
