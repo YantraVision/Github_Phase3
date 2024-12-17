3be65cd75ec3914fdb599b47bbae2c34387c7e26
kishoreV85 <kishore.v@yantravision.com>
2024-12-17 15:20:13 +0530

    Included Subtraction

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include<stdio.h>
 
 int add(int input_1,int input_2); //Function to add two numbers
+int subtraction(int input_1,int input_2); //Function to subtraction of two number
 
 int main(){
 int input_1=30;
@@ -9,6 +10,8 @@ int input_2=2;
 int result=0;
 result=add(input_1,input_2);
 printf("Addition of two inputs=%d",&result);
+result=subtraction(input_1,input_2);
+printf("Subtraction of two inputs=%d",&result);
 return 0;
 }
 
@@ -17,6 +20,13 @@ int result=0;
 result=input_1+input_2;
 return result;
 }
+
+int subtraction(int input_1,int input_2){
+int result=0;
+result=input_1-input_2;
+return result;
+}
+
 
 
    
