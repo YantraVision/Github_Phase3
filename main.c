8f52e27e8deb139c250f046ecf8f0ed68d67da0d
Hariharan73958 <hariharan.s@yantravision.com>
2024-12-17 15:29:14 +0530

    Multiplication by Hariharan

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 
 int add(int input_1,int input_2); //Function to add two numbers
 int subtraction(int input_1,int input_2); //Function to subtraction of two number
-
+int multiplication(int input_1,int input_2); //Function to Multiply
 int main(){
 int input_1=30;
 int input_2=2;
@@ -12,6 +12,8 @@ result=add(input_1,input_2);
 printf("Addition of two inputs=%d",&result);
 result=subtraction(input_1,input_2);
 printf("Subtraction of two inputs=%d",&result);
+result=multiplication(input_1,input_2);
+printf("Multiplication of two inputs=%d",&result);
 return 0;
 }
 
@@ -27,6 +29,10 @@ result=input_1-input_2;
 return result;
 }
 
-
+int multiplication(int input_1,int input_2){
+int result=0;
+result=input1*input2;
+return result;
+}
 
    
