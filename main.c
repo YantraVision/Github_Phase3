848c20004303c35f502c6fd71f0a181717d7039f
Hariharan73958 <hariharan.s@yantravision.com>
2024-12-18 15:36:47 +0530

    Errors are corrected

--- main.c
+++ main.c
@@ -11,11 +11,11 @@ int input_1=30;
 int input_2=2;
 int result=0;
 result=add(input_1,input_2);
-printf("Addition of two inputs=%d",&result);
+printf("Addition of two inputs=%d",result);
 result=subtraction(input_1,input_2);
-printf("Subtraction of two inputs=%d",&result);
+printf("Subtraction of two inputs=%d",result);
 result=multiplication(input_1,input_2);
-printf("Multiplication of two inputs=%d",&result);
+printf("Multiplication of two inputs=%d",result);
 result=division(input_1,input_2);
 printf("Division of two inputs=%d",result);
 return 0;
@@ -35,12 +35,16 @@ return result;
 
 int multiplication(int input_1,int input_2){
 int result=0;
-result=input1*input2;
+result=input_1*input_2;
 return result;
 }
 
 int division(int input_1,int input_2){
 int result=0;
-result=input_1/input2;
+result=input_1/input_2;
 return result;
+
+
+printf("After review i am editing this bu using clone");
+
 }   
